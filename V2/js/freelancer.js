5782963f2b0c34f74aa7ca25c3f8c4ed985dc086
Sam Garg <samgarg86@gmail.com>
2016-03-28 23:54:20 -0500

    Kleenbeats and alessandro iovino added, resume updated..

--- V2/js/freelancer.js
+++ V2/js/freelancer.js
@@ -38,44 +38,23 @@ $('.navbar-collapse ul li a').click(function() {
 
 
 var myApp = angular.module('samgargsWebsite', []);
-myApp.controller('PortfolioController', function($scope, ProjectsFactory){
-    function init(){
+myApp.controller('PortfolioController', function($scope, ProjectsFactory) {
+    function init() {
         ProjectsFactory.getProjects()
-            .success(function(data){
+            .success(function(data) {
                 $scope.projects = data;
             })
-            .error(function(data, status){
+            .error(function(data, status) {
                 alert(status);
             });
     }
+
     init();
-})
-    .factory('ProjectsFactory', function($http){
-        var factory = {}
-        factory.getProjects = function(){
-            return $http.get('projects.json')
-        }
-        return factory;
-    });
-    //.directive('projectsRenderedDirective', function(){
-    //    return function(scope, element, attrs){
-    //        if(scope.$last){
-    //            element.parent().mixitup({
-    //                targetSelector: '.portfolio',
-    //                filterSelector: '.filter',
-    //                effects: ['fade'],
-    //                easing: 'snap'
-    //            });
-    //        }
-    //        element.hover(
-    //            function () {
-    //                $(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
-    //                $(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');
-    //            },
-    //            function () {
-    //                $(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
-    //                $(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');
-    //            }
-    //        );
-    //    };
-    //});
+}).factory('ProjectsFactory', function($http) {
+    var factory = {}
+    factory.getProjects = function() {
+        return $http.get('projects.json')
+    }
+    return factory;
+});
+
