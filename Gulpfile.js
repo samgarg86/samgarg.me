c73fea20dc6ee6e92fe728d12d52c924bfb132dd
Sam Garg <samgarg86@gmail.com>
2016-08-22 21:02:19 -0500

    Some SEO fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
-	gulp.src('./scss/**/*.scss')
+var input = './scss/**/*.scss';
+var output = './css/';
+
+
+gulp.task('compile-css', function() {
+	return gulp
+		.src(input)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(output));
 });
 
 //Watch task
-gulp.task('default',function() {
-	// run a build first
-	gulp.src('./scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
-
+gulp.task('default', ['compile-css'], function() {
 	// watch for any changes
-	gulp.watch('./scss/**/*.scss',['styles']);
+	return gulp.watch(input,['compile-css']);
 });
+
