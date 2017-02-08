0bdc8d8de6fef2e40f2b227e5c9a3f5f831e1e55
Sam Garg <samgarg86@gmail.com>
2017-02-08 00:40:00 +0000

    Updated resume 2017, added gulp tasks for building resume CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,49 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 
 var input = './css/scss/**/*.scss';
 var output = './css/';
 
+var inputResume = './resume/css/scss/**/*.scss';
+var outputResume = './resume/css/';
 
-gulp.task('compile-css', function() {
+
+gulp.task('css-main', function() {
 	return gulp
 		.src(input)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
+		// .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(gulp.dest(output));
 });
 
+gulp.task('css-resume', function() {
+    return gulp
+        .src(inputResume)
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(autoprefixer())
+        // .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(outputResume));
+});
+
+//Watch Resume
+gulp.task('css-resume-watch', ['css-resume'], function() {
+    // watch for any changes
+    return gulp.watch(inputResume,['css-resume']);
+});
+
+//Watch Main
+gulp.task('css-main-watch', ['css-main'], function() {
+    // watch for any changes
+    return gulp.watch(input,['css-main']);
+});
+
 //Watch task
-gulp.task('default', ['compile-css'], function() {
+gulp.task('default', ['css-main'], function() {
 	// watch for any changes
-	return gulp.watch(input,['compile-css']);
+	return gulp.watch(input,['css-main']);
 });
 
